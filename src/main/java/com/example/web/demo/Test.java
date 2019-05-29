a882c51059b79f5e231bc4be801bb2ac941629d8
xiaoxiao <365608783@qq.com>
2019-05-30 06:43:47 +0800

    Initial commit

---
+++ src/main/java/com/example/web/demo/Test.java
@@ -0,0 +1,20 @@
+package com.example.web.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Auther: 36560
+ * @Date: 2019/5/30 :6:40
+ * @Description:
+ */
+@Controller
+@RequestMapping("/")
+public class Test {
+    @RequestMapping("/test")
+    @ResponseBody
+    public String getResult(){
+        return "我是一个应用页面";
+    }
+}
